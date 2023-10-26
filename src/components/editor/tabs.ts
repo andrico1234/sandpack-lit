6dab17fe46d13356c36b0023c010bc0ccb7cd6fb
RougeCode007 <abioyefeyifoluwa@gmail.com>
2023-10-26 09:08:54 +0100

    add fix

--- src/components/editor/tabs.ts
+++ src/components/editor/tabs.ts
@@ -4,8 +4,9 @@ import { sandpackContext } from "../../contexts/context.js";
 import type { SandpackContext } from "../../contexts/context.js";
 import { consume } from "@lit-labs/context";
 import { when } from "lit/directives/when.js";
+import { closeIcon } from "../icons/close.js";
 
-@customElement('sandpack-tabs')
+@customElement("sandpack-tabs")
 class Tabs extends LitElement {
   static styles = css`
     :host {
@@ -50,10 +51,10 @@ class Tabs extends LitElement {
     li[data-active] button {
       opacity: 1;
     }
-  `
+  `;
 
   @property()
-  closeableTabs = false
+  closeableTabs = false;
 
   // @ts-ignore
   @consume({ context: sandpackContext, subscribe: true })
@@ -61,12 +62,12 @@ class Tabs extends LitElement {
 
   updateActiveFile(fileName: string) {
     if (fileName !== this.sandpack.activeFile) {
-      this.sandpack.onActiveFileChange(fileName)
+      this.sandpack.onActiveFileChange(fileName);
     }
   }
 
   closeFile(fileName: string) {
-    this.sandpack.onFileClose(fileName)
+    this.sandpack.onFileClose(fileName);
   }
 
   render() {
@@ -74,20 +75,22 @@ class Tabs extends LitElement {
     const openFiles = this.sandpack.openFiles;
 
     return html`<ul id="tabs">
-      ${openFiles.map((file) => html`<li ?data-active=${activeFile === file}>
-        <button @click=${() => this.updateActiveFile(file)}>
-          ${file}
-        </button>
-        ${when(this.closeableTabs, () => html`
-        <button @click = ${() => this.closeFile(file)} aria-label="close">
-          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>
-        </button>
-        `)}
-      </li>`
-    )
-      }
-  </ul>`
+      ${openFiles.map(
+        (file) =>
+          html`<li ?data-active=${activeFile === file}>
+            <button @click=${() => this.updateActiveFile(file)}>${file}</button>
+            ${when(
+              this.closeableTabs,
+              () => html`
+                <button @click=${() => this.closeFile(file)} aria-label="close">
+                  ${closeIcon}
+                </button>
+              `
+            )}
+          </li>`
+      )}
+    </ul>`;
   }
 }
 
-export default Tabs
+export default Tabs;
